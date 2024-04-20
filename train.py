1e60adaf6ff6551ad72ff31464dc261a1524354d
Sandeep_Pulavarthi <sandeep.pulavarthi17@gmail.com>
2024-04-20 19:02:51 +0530

    Adding error in train.py file

--- train.py
+++ train.py
@@ -6,7 +6,7 @@ import numpy as np
 df = pd.read_csv("data/train.csv")
 X = df.drop(columns=['Disease']).to_numpy()
 y = df['Disease'].to_numpy()
-labels = np.sort(np.unique(y))
+labels = np.sorting(np.unique(y))
 y = np.array([np.where(labels == x) for x in y]).flatten()
 
 model = LogisticRegression().fit(X, y)
